eff30b774cd604673eaa0c0678584169ab269b35
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-02-20 12:49:42 +0200

    add components

--- Et-Putki_angular/src/app/app.module.ts
+++ Et-Putki_angular/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainImageComponent } from './main-image/main-image.component';
+import { InfoSectionComponent } from './info-section/info-section.component';
+import { ServicesSectionComponent } from './services-section/services-section.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainImageComponent,
+    InfoSectionComponent,
+    ServicesSectionComponent,
+    ContactFormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
